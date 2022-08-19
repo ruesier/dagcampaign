39f8ddb35cfce076b80f76ac7634cad4332db814
Devon Call <callbrentdevon@gmail.com>
2022-08-19 15:09:11 -0700

    Add go-graphviz

--- dag.go
+++ dag.go
@@ -1,2 +1,16 @@
 package dagcampaign
 
+import (
+	"errors"
+	"io"
+
+	"github.com/goccy/go-graphviz"
+)
+
+type Campaign struct {
+	Name string
+}
+
+func (c *Campaign) Render(format graphviz.Format, w io.Writer) error {
+	return errors.New("no implemented")
+}
